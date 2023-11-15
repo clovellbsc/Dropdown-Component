55a4f5a735a00408867fae4e61308c43a3ce976c
Chris <clovellbsc@gmail.com>
2023-11-15 15:26:34 +0000

    updating multi logic

--- src/hooks/useMultiDropdown.tsx
+++ src/hooks/useMultiDropdown.tsx
@@ -12,46 +12,52 @@ function useMultiDropdown({
 }) {
   const [selectedItems, setSelectedItems] = useState<IObjectItem[]>([])
 
-  const handleSelection = useCallback((item: IObjectItem | null) => {
-    item !== null
-      ? setSelectedItems((prev) => [...prev, item])
-      : setSelectedItems([])
-    if (selectRef?.current?.children) {
-      const childArray = Array.from(selectRef.current.children)
-      childArray.forEach((child) => {
-        const option = child as HTMLOptionElement
-        if (item === null) {
-          option.selected = false
-          return
-        }
-        if (option.value === item.value) {
-          option.selected === true
-            ? handleDeselection(item)
-            : (option.selected = true)
-          return
-        }
-      })
-      selectRef.current.dispatchEvent(new Event('change', { bubbles: true }))
-    }
-    dropdownRef.current?.blur()
-    inputRef.current?.blur()
-  }, [])
+  const handleDeselection = useCallback(
+    (item: IObjectItem) => {
+      setSelectedItems((prev) =>
+        prev.filter((selectedItem) => selectedItem.value !== item.value)
+      )
+      if (selectRef?.current?.children) {
+        const childArray = Array.from(selectRef.current.children)
+        childArray.forEach((child) => {
+          const option = child as HTMLOptionElement
+          if (option.value === item.value) {
+            option.selected = false
+          }
+        })
+        selectRef.current.dispatchEvent(new Event('change', { bubbles: true }))
+      }
+    },
+    [selectRef]
+  )
 
-  const handleDeselection = (item: IObjectItem) => {
-    setSelectedItems((prev) =>
-      prev.filter((selectedItem) => selectedItem.value !== item.value)
-    )
-    if (selectRef?.current?.children) {
-      const childArray = Array.from(selectRef.current.children)
-      childArray.forEach((child) => {
-        const option = child as HTMLOptionElement
-        if (option.value === item.value) {
-          option.selected = false
-        }
-      })
-      selectRef.current.dispatchEvent(new Event('change', { bubbles: true }))
-    }
-  }
+  const handleSelection = useCallback(
+    (item: IObjectItem | null) => {
+      item !== null
+        ? setSelectedItems((prev) => [...prev, item])
+        : setSelectedItems([])
+      if (selectRef?.current?.children) {
+        const childArray = Array.from(selectRef.current.children)
+        childArray.forEach((child) => {
+          const option = child as HTMLOptionElement
+          if (item === null) {
+            option.selected = false
+            return
+          }
+          if (option.value === item.value) {
+            option.selected === true
+              ? handleDeselection(item)
+              : (option.selected = true)
+            return
+          }
+        })
+        selectRef.current.dispatchEvent(new Event('change', { bubbles: true }))
+      }
+      dropdownRef.current?.blur()
+      inputRef.current?.blur()
+    },
+    [dropdownRef, inputRef, selectRef, handleDeselection]
+  )
 
   const handleRemoveAllSelected = () => {
     setSelectedItems([])
